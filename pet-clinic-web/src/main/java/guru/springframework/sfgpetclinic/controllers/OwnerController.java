94a21beaa10f285ff89f5dd7eae6f16e4aafff5c
David Bates <david_bates@mac.com>
2020-08-13 13:52:00 -0700

    after 67

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VetController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
-        return "vets/index";
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwners() {
+        return "owners/index";
     }
 }

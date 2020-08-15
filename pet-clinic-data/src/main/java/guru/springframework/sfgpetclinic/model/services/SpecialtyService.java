96094aa042fc6dd5432d36e93fe346d8c27e4f59
David Bates <david_bates@mac.com>
2020-08-15 15:19:44 -0700

    added specialtyservice and map imple

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.model.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SpecialtyService extends CrudService<Specialty, Long>{
+
+}

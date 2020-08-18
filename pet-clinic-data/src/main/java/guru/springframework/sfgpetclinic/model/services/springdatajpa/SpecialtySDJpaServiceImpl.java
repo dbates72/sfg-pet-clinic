bba01f5722a2bcb11bb7d4292cf11c41490edb69
David Bates <david_bates@mac.com>
2020-08-18 13:00:57 -0700

    implment PetType and Speciality JPA impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/springdatajpa/SpecialtySDJpaServiceImpl.java
@@ -0,0 +1,47 @@
+package guru.springframework.sfgpetclinic.model.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.model.repositories.SpecialtyRepository;
+import guru.springframework.sfgpetclinic.model.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaServiceImpl implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaServiceImpl(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties =  new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
